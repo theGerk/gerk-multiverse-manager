922fa8a37929323380f65fc1583bde14f25c9c5d
Benji Altman <benji@altmansoftwaredesign.com>
2021-12-29 17:22:44 -0600

    ???

---
+++ src/main/java/net/gerk/multiverse/GerkMultiverse.java
@@ -0,0 +1,12 @@
+package net.gerk.multiverse;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GerkMultiverse implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+		System.out.println("Hello world");
+	}
+}
